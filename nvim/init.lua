65d4ccc86ffa94e8ebae7cd02357b4808ce2b960
lafita_v <lafita_v@etna-alternance.net>
2024-11-05 09:46:08 +0100

    [MOD] broke up configuration into separate plugins file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,36 +1,21 @@
 require("user.options")
+require("user.colorscheme")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-	{'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	{
-	"nvim-neo-tree/neo-tree.nvim",
-    	branch = "v3.x",
-    	dependencies = {
-      	"nvim-lua/plenary.nvim",
-      	"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      	"MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    		}
-	}	
-}
 
-local opts = {}
 
-require("lazy").setup(plugins, opts)
-require("user.colorscheme")
+require("lazy").setup("plugins")
 local builtin = require("telescope.builtin")
 vim.keymap.set('n' , '<C-p>', builtin.find_files, {})
 vim.keymap.set('n' , '<C-t>', ':Neotree filesystem reveal right <CR>')
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,14 @@
+return  {
+	{'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	{
+	"nvim-neo-tree/neo-tree.nvim",
+    	branch = "v3.x",
+    	dependencies = {
+      	"nvim-lua/plenary.nvim",
+      	"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      	"MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    		}
+	} 	
+}
